7715999bb7bcd95abb3500c98d691047af712a18
Jia-Jingnan <875480307@qq.com>
2021-06-10 23:27:28 +0800

    0610-新增HomeController类，完成查询首页帖子的方法

---
+++ src/main/java/com/lilith/brainly/controller/HomeController.java
@@ -0,0 +1,47 @@
+package com.lilith.brainly.controller;
+
+import com.lilith.brainly.entity.DiscussPost;
+import com.lilith.brainly.entity.User;
+import com.lilith.brainly.service.DiscussPostService;
+import com.lilith.brainly.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 下午11:18 2021/6/10
+ */
+@Controller
+public class HomeController {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String getIndexPage(Model model){
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, 1, 10);
+        List<Map<String,Object>> discussPosts = new ArrayList<>();
+        if (list != null){
+            for (DiscussPost discussPost : list){
+                Map<String,Object> map = new HashMap<>();
+                map.put("post",discussPost);
+                User user = userService.findUserById(discussPost.getUserId());
+                map.put("user",user);
+                discussPosts.add(map);
+            }
+        }
+        model.addAttribute("discussPosts",discussPosts);
+        return "/index";
+    }
+}
